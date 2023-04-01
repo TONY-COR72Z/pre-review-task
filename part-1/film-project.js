b948dc9f948d8aabcbda0f00f2f8239181a57d2c
Anton Yusupov <an87yu@gmail.com>
2023-04-01 21:33:23 +0300

    part-1 done

--- part-1/film-project.js
+++ part-1/film-project.js
@@ -1,6 +1,27 @@
 class FilmProject {
   constructor(args = {}) {
     this.title = args.title;
+    this.director = args.director;
+    this.cast = args.cast;
+    this.castingBudget = args.castingBudget;
+  }
+
+  remainingBudget() {
+    const sal = this.cast.map((item) => item.salary);
+    return this.castingBudget - sal.reduce((acc, num) => acc + num, 0);
+  }
+
+  addActor(actor) {
+    if (actor.salary <= this.castingBudget) {
+      this.cast.push(actor);
+      return true;
+    }
+    return false;
+  }
+
+  headliners() {
+    const headliners = this.cast.filter((item) => item.popularityRating > 8);
+    return headliners;
   }
 }
 
