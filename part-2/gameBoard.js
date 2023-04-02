4720c90fa6940fca7ded0fd77992c7aeabd73024
Anton Yusupov <an87yu@gmail.com>
2023-04-02 12:56:16 +0300

    part-2 done

--- part-2/gameBoard.js
+++ part-2/gameBoard.js
@@ -7,10 +7,27 @@ class GameBoard {
 
   terrainMap() {
     // напишите этот метод сами
+    const convertList = this.terrainList.map(this.terrainHelper.letterToTerrain);
+    const gBoard = [];
+    for (let i = 0; i < this.size; i += 1) {
+      const row = [];
+      for (let j = 0; j < this.size; j += 1) {
+        row.push(convertList[i * this.size + j]);
+      }
+      gBoard.push(row);
+    }
+    return gBoard;
   }
 
-  difficultyOfJourney() {
+  difficultyOfJourney(way) {
     // напишите этод метод сами
+    let score = 0;
+    for (let i = 0; i < way.length; i += 1) {
+      const w = this.terrainMap()[way[i][0]][way[i][1]];
+      // console.log(w);
+      score += this.terrainHelper.scoreDifficulty(w);
+    }
+    return score;
   }
 }
 
