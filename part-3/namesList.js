436319d49a20342e76b901f15cabf964bd8aa433
Anton Yusupov <an87yu@gmail.com>
2023-04-02 21:15:35 +0300

    part-3 done

--- part-3/namesList.js
+++ part-3/namesList.js
@@ -1,11 +1,17 @@
 const fs = require('fs/promises');
 const fetchUsers = require('./fetchUsers');
 
-function fetchNames() {}
+function fetchNames() {
+  return fetchUsers().then((data) => data.map((elem) => elem.name));
+}
 
-function saveNames(names) {}
+function saveNames(names) {
+  return fs.writeFile('./names.txt', names.join('\n'), 'utf-8');
+}
 
-function getName(count) {}
+function getName(count) {
+  return fs.readFile('./names.txt', 'utf-8').then((data) => data.split('\n')[count - 1]);
+}
 
 module.exports = {
   fetchNames,
